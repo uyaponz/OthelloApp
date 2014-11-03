0ccdb1f87a5f6529357fdef84de72c18aa14de11
uyaponz <uyaponz@gmail.com>
2014-11-04 02:38:05 +0900

    othello のページへの routes を作成、仮のページを作成

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,5 +8,9 @@ object Application extends Controller {
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
+  
+  def othello = Action {
+    Ok(views.html.othello())
+  }
 
 }
